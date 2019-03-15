d813d2c10171aa8d8c16408d89b1fd6e9f5a8b84
vivian stephan carpin <viviancarpin@gmail.com>
2019-03-16 02:54:54 +0400

    Updates

---
+++ precache-manifest.8c2ba3e21ce5a99c5f86e9816d51dcde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1bc2983e6860e9ae51a",
+    "url": "/react-gh-pages/static/css/main.fa5fe999.chunk.css"
+  },
+  {
+    "revision": "c1bc2983e6860e9ae51a",
+    "url": "/react-gh-pages/static/js/main.c125aeb9.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-gh-pages/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3648034c545cba46ec68b65e690bfdd1",
+    "url": "/react-gh-pages/index.html"
+  }
+];
